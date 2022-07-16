e3b1e510e7ab8f3c7cf9eac53818d488a5e6ac36
AnonymousSudh <mauryasudhanshu930@gmail.com>
2022-07-16 19:41:06 +0530

    all 1

--- src/Newsapi.jsx
+++ src/Newsapi.jsx
@@ -29,8 +29,8 @@ function Newsapi() {
         //     }
         // })
         // console.log("3");
-        console.log(json_data.articles);
-        setnews(json_data.articles)
+        // console.log(json_data.articles);
+        setnews(json_data.articles[0])
 
     }
 
@@ -41,9 +41,9 @@ function Newsapi() {
 
     return (
         <>
-        {/* <h1>{news[0].title}</h1> */}    
+        <h1>{news.title}</h1>    
         
-            {news.map((val) => {
+            {/* {news.map((val) => {
                 return (
                     <>
                         <div className="newscard">
@@ -63,7 +63,7 @@ function Newsapi() {
 
                     </>
                 )
-            })}
+            })} */}
 
 
         </>
