62fb5e36fc259ed87d49d451b14609406b7d04c1
AnonymousSudh <mauryasudhanshu930@gmail.com>
2022-07-16 19:21:46 +0530

    all donne

---
+++ src/Newsapi.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+// import NewsCard from './card/NewsCard';
+import('./newsapi.css')
+
+
+function Newsapi() {
+
+    const [news, setnews] = useState([]);
+
+    const fetchNews = async () => {
+
+        const url2 = `https://newsapi.org/v2/everything?q=agriculture&sortBy=popularity&apiKey=870ca578389942988efdd66eb6b53c42`
+
+        // console.log("0");
+        const data = await fetch(url2);
+        // console.log("1");
+        const json_data = await data.json();
+
+        var array = []
+        const filterdata = json_data.articles.filter((elem, index) => {
+            // console.log(elem.title);
+            const string = elem.title
+            const result = string.includes("India's")
+            const result2 = string.includes("India")
+            // console.log(result);
+            if (result == true || result2 == true) {
+                array.push(elem)
+            }
+        })
+        console.log("3");
+        console.log(array);
+        setnews(array)
+
+    }
+
+    useEffect(() => {
+        fetchNews();
+
+    }, [])
+
+    return (
+        <>
+        {/* <h1>{news[0].title}</h1> */}    
+        
+            {news.map((val) => {
+                return (
+                    <>
+                        <div className="newscard">
+
+                            <div className="newscardholder">
+
+                                <div className="newsimg">
+                            
+                                </div>
+                                <h1>{val.title}</h1>
+                                <hr />
+                                <div className='news_disc'>
+                                    <h2>{val.description}</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                    </>
+                )
+            })}
+
+
+        </>
+    )
+}
+
+export default Newsapi
